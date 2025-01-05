3ef37448574644dbee7608466e324127cab24321
Purvi90 <purvisharma1239@gmail.com>
2025-01-05 22:31:20 +0530

    upload file

---
+++ src/components/Random.js
@@ -0,0 +1,24 @@
+import React , {useState} from 'react'
+
+export const Random = () => {
+
+    const[gif , setGif] =useState('');
+
+    function clickHandler(){
+
+
+    }
+    
+  return (
+    <div>
+   <h1>Random Gif</h1>
+   <img src={gif} width="450"/>
+
+   <button 
+   onClick={clickHandler}>
+     Generate
+    </button>
+    </div>
+  )
+}
+export default Random
---
+++ src/components/Tag.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Tag = () => {
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+export default Tag
