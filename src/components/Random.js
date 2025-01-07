3b07f334f1085ecd988a06ac99fd86ea83abaded
Purvi90 <purvisharma1239@gmail.com>
2025-01-07 22:19:27 +0530

    changes

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,8 +1,12 @@
 import React , {useState} from 'react'
 
-export const Random = () => {
 
+
+  const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+
+  const Random = () => {
     const[gif , setGif] =useState('');
+    const url =
 
     function clickHandler(){
 
@@ -10,17 +14,23 @@ export const Random = () => {
     }
     
   return (
+    
     <div className='w-1/2 h-[450px] bg-green-500 rounded-lg border border-black
     flex flex-col items-center gap-y-5 mt-[15px]'>
+   
    <h1 className='text-2xl underline uppercase font-bold'> A Random Gif</h1>
+  
    <img src={gif} width="450"/>
 
    <button 
+ 
    onClick={clickHandler}
    className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg">
      Generate
+   
     </button>
-    </div>
+    
+     </div>
   )
 }
 export default Random
