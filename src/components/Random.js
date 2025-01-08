846dd8e47909dc7fe12a9ed6b36fc142202fd795
Purvi90 <purvisharma1239@gmail.com>
2025-01-08 22:55:13 +0530

    changes

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,31 +1,51 @@
 import React , {useState} from 'react'
+import axios from 'axios';
+import {useEffect} from 'react';
+import Spinner from './Spinner';
 
 
 
   const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
   const Random = () => {
-    const[gif , setGif] =useState('');
-    const url =
 
+    const[gif , setGif] = useState('');
+    const [loading,setLoading]=useState('false');
+
+    async function fetchData (){
+
+      setLoading(true);
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+    const {data}  = await axios.get(url);
+  const imageSource = data.data.images.downsized_large.url;
+ setGif(imageSource);
+ setLoading(false);
+}
+  useEffect ( () => {
+    fetchData();
+  },[] )
     function clickHandler(){
 
+  fetchData();
 
     }
     
   return (
     
-    <div className='w-1/2 h-[450px] bg-green-500 rounded-lg border border-black
+    <div className='w-1/2  bg-green-500 rounded-lg border border-black
     flex flex-col items-center gap-y-5 mt-[15px]'>
    
-   <h1 className='text-2xl underline uppercase font-bold'> A Random Gif</h1>
+   <h1 className='mt-[15px] text-2xl underline uppercase font-bold '> A Random Gif</h1>
   
-   <img src={gif} width="450"/>
+  {
+    loading ? ( <Spinner/>) :(<img src = {gif} width ="450"/>)
+  }
+   
 
    <button 
  
    onClick={clickHandler}
-   className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg">
+   className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]">
      Generate
    
     </button>
@@ -33,4 +53,4 @@ import React , {useState} from 'react'
      </div>
   )
 }
-export default Random
+export default Random;
