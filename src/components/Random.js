b496433554acf7e115f45364f8da87d395a7cba0
Purvi90 <purvisharma1239@gmail.com>
2025-01-10 16:30:23 +0530

    final changes

--- src/components/Random.js
+++ src/components/Random.js
@@ -2,33 +2,16 @@ import React , {useState} from 'react'
 import axios from 'axios';
 import {useEffect} from 'react';
 import Spinner from './Spinner';
-
+import useGif from '../hooks/useGif';
 
 
   const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
   const Random = () => {
 
-    const[gif , setGif] = useState('');
-    const [loading,setLoading]=useState('false');
-
-    async function fetchData (){
-
-      setLoading(true);
-    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-    const {data}  = await axios.get(url);
-  const imageSource = data.data.images.downsized_large.url;
- setGif(imageSource);
- setLoading(false);
-}
-  useEffect ( () => {
-    fetchData();
-  },[] )
-    function clickHandler(){
-
-  fetchData();
-
-    }
+  const{gif , loading , fetchData}= useGif();
+   
+    
     
   return (
     
@@ -42,9 +25,7 @@ import Spinner from './Spinner';
   }
    
 
-   <button 
- 
-   onClick={clickHandler}
+   <button  onClick ={()=> fetchData()}
    className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]">
      Generate
    
--- src/components/Tag.js
+++ src/components/Tag.js
@@ -2,6 +2,7 @@ import React , {useState} from 'react'
 import axios from 'axios';
 import {useEffect} from 'react';
 import Spinner from './Spinner';
+import useGif from '../hooks/useGif';
 
 
 
@@ -9,33 +10,18 @@ import Spinner from './Spinner';
 
   const Tag = () => {
 
-    const[gif , setGif] = useState('');
-    const [loading,setLoading]=useState('false');
+    const [tag , setTag]=useState('car');
+  
+    const{gif , loading , fetchData}=useGif(tag);
 
-    async function fetchData (){
-
-      setLoading(true);
-    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-    const {data}  = await axios.get(url);
-  const imageSource = data.data.images.downsized_large.url;
- setGif(imageSource);
- setLoading(false);
-}
-  useEffect ( () => {
-    fetchData();
-  },[] )
-    function clickHandler(){
-
-  fetchData();
-
-    }
-    
+   
+  
   return (
     
     <div className='w-1/2  bg-blue-500 rounded-lg border border-black
     flex flex-col items-center gap-y-5 mt-[15px]'>
    
-   <h1 className='mt-[15px] text-2xl underline uppercase font-bold '>  Random Gif</h1>
+   <h1 className='mt-[15px] text-2xl underline uppercase font-bold '>  Random  {tag} Gif</h1>
   
   {
     loading ? ( <Spinner/>) :(<img src = {gif} width ="450"/>)
@@ -44,12 +30,13 @@ import Spinner from './Spinner';
 
 <input 
 
-className='w-10/12 text-lg py-2 rounded-lg mb-[20px]'
+className='w-10/12 text-lg py-2 rounded-lg mb-[3px] text-center'
+
+ onChange={(event)=> setTag(event.target.value)}
+value={tag}
 />
 
-   <button 
- 
-   onClick={clickHandler}
+   <button onClick={() => fetchData(tag)}
    className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]">
      Generate
    
